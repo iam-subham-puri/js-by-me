ff1881ece083c8178666c57ac03fba88ccf78cc6
iam-subham-puri <behera.subham004@gmail.com>
2025-07-11 16:01:31 +0000

    conversion of datetype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+let score = null
+
+console.log(typeof score);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber)
+
+//"33"- 33
+// "33anb" - NaN
+//true - 1;false - 0;
+
+//1- true ,0 - fasle
+//"" - false
+// "asds" - true
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)//string
