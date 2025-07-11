6b860556aebf06007a99bf44e1bef0849f245fe8
iam-subham-puri <behera.subham004@gmail.com>
2025-07-11 15:18:44 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144
+let accountEmail = "subham@gmail.com"
+var accountPassword = "12345"
+accountCity = "puri"
+let accountState;
+
+// accountId = 2 // not allowed
+
+accountEmail = "hh@gmail.com"
+accountPassword = "swwcw"
+accountCity = "bhubaneswar"
+console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity,accountState]);
