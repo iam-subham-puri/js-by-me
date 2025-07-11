162beb0274e3e6961861bb5b240c31fcbd32a577
iam-subham-puri <behera.subham004@gmail.com>
2025-07-11 15:34:57 +0000

    detatype

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat aii js code as newer version
+
+//alert(3 + 3) // we are using nodesjs,not browser
+
+
+
+let name = "subham"
+let age = 3
+let isLoggedIn =false
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); //object
